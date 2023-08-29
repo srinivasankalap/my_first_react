8fa75e5897035353af496c97d8e92bceb4b805c9
srinivasankalap <cnu.ankalap@gmail.com>
2023-08-29 11:37:49 +0530

    Hard Code

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+    <div>
+        <h6>Food Rs 10</h6>
+        <h6>Petrol Rs 100</h6>
+        <h6>Movie Rs 200</h6>
+    </div>
+    )
+}
+
+export default ExpenseItem;
