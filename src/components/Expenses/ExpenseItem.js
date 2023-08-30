616d333f5e345d1851f3c10f67481c31f46913a5
srinivasankalap <cnu.ankalap@gmail.com>
2023-08-30 16:41:16 +0530

    Change Expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,34 @@
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 import Card from "../UI/Card";
 
 const ExpenseItem=(props)=>{
+    const [title,newTitle]=useState(props.title);
+    const [cost,newCost]=useState(props.amount);
+
     const clickHandler=()=>{
+        newTitle("Updated..");
         console.log('Clicked!');
     }
 
     const Deleting=()=>{
-        console.log("Delete Clicked!");
-    }
-    document.getElementById('root').addEventListener("click",Deleting);
+        newCost(100);
+        };
+    
 
     return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__description'><h2>{props.location}</h2></div>
-            <div className='expense-item__price'>${props.amount}</div>
+            <div className='expense-item__price'>${cost}</div>
         </div>
         <button onClick={clickHandler}>Change Title</button>
-        <button>Delete Expense</button>
+        <button onClick={Deleting}>Change Expense</button>
     </Card>
     );
-}
+    }
 
 export default ExpenseItem;
