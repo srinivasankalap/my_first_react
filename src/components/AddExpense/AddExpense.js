ae32846c407f63bafc149ab52a6fa38390936899
srinivasankalap <cnu.ankalap@gmail.com>
2023-09-04 15:39:38 +0530

    Collapsing Form

--- src/components/AddExpense/AddExpense.js
+++ src/components/AddExpense/AddExpense.js
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import './AddExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const AddExpense=(props)=>{
+    const [adding, setAdding]=useState(false);
     const savingExpenseData=(inputExpenseData)=>{
         const expenseData={
             ...inputExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setAdding(false);
+    }
+    const startAdding=()=>{
+        return setAdding(true);
+    }
+    const stopAdding=()=>{
+        return setAdding(false);
     }
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={savingExpenseData}/>
+            {!adding && (
+                <button onClick={startAdding}>Add New Expense</button>
+            )}
+            {adding && (
+                <ExpenseForm 
+                    onSaveExpenseData={savingExpenseData}
+                    onCancel={stopAdding}
+                />
+            )}
+            
         </div>
     )
 }
