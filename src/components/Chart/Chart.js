a2609045b4e9fcd2bb96328e320aefc64f5f041f
srinivasankalap <cnu.ankalap@gmail.com>
2023-09-04 16:25:40 +0530

    Chart

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,18 @@
+import ChartBar from './ChartBar'
+import './Chart.css'
+
+const Chart=(props)=>{
+    const dataPointArray=props.dataPoints.map(dataPoint=> (dataPoint.value));
+    const totalMax=Math.max(...dataPointArray); 
+
+    return <div className='chart'>
+        {props.dataPoints.map((dataPoint) =>(
+            <ChartBar 
+                key={dataPoint.label}
+                value={dataPoint.value} 
+                maxValue={totalMax} 
+                label={dataPoint.label}/>
+        ))}
+    </div>
+}
+export default Chart;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import './Expenses.css';
 import Filter from './Filter';
 import Card from "../UI/Card";
 import ExpenseList from "./ExpenseList";
+import ExpensesChart from './ExpensesChart';
 import { useState } from "react";
 
 const Expenses=(props)=>{
@@ -18,6 +19,7 @@ const Expenses=(props)=>{
       <li>
     <Card className="expenses">
       <Filter selected={year} onChangeFilter={filterYear}/>
+      <ExpensesChart expenses={filteredArray}/>
       <ExpenseList items={filteredArray}/>
     </Card>
     </li>
