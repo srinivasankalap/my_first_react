a1b517f1671146b80a19c422b5eaaf4001eaea58
srinivasankalap <cnu.ankalap@gmail.com>
2023-09-04 12:42:09 +0530

    Filter and making Dynamic Array

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Filter from './Filter';
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const Expenses=(props)=>{
+    const [year, setYear]=useState('2023');
+    const filterYear=(selectedYear)=>{
+      setYear(selectedYear);
+    }
 
     return (
     <Card className="expenses">
+      <Filter selected={year} onChange={filterYear}/>
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
